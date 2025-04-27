c815350b1481574d2be7876bc8cb49098b626c36
John Walker II <john.walker.ii@gmail.com>
2025-04-27 10:55:21 -0700

    MISC: Updated createRandomIP to use the full 32 bit space instead of ~16. ip addresses that only matched xx.x.x.x was not a realistic range.

--- src/utils/IPAddress.ts
+++ src/utils/IPAddress.ts
@@ -1,10 +1,10 @@
 import type { IPAddress } from "../Types/strings";
-import { getRandomByte } from "./helpers/getRandomByte";
 
 /**
  * Generate a random IP address
  * Does not check to see if the IP already exists in the game
  */
 export const createRandomIp = (): IPAddress => {
-  return `${getRandomByte(99)}.${getRandomByte(9)}.${getRandomByte(9)}.${getRandomByte(9)}` as IPAddress;
+  const ip = (Math.random() * 0xffffffff) & 0xffffffff;
+  return `${(ip >>> 24) & 0xff}.${(ip >>> 16) & 0xff}.${(ip >>> 8) & 0xff}.${ip & 0xff}` as IPAddress;
 };
