dd1a297f35ffd839cd032e3ea08f18de22be4873
John Walker II <john.walker.ii@gmail.com>
2025-04-27 20:55:07 -0700

    Refactored using simpler to understand formula, and explained in comments as per CatLover's suggestion

--- src/utils/IPAddress.ts
+++ src/utils/IPAddress.ts
@@ -5,6 +5,15 @@ import type { IPAddress } from "../Types/strings";
  * Does not check to see if the IP already exists in the game
  */
 export const createRandomIp = (): IPAddress => {
-  const ip = (Math.random() * 0xffffffff) & 0xffffffff;
-  return `${(ip >>> 24) & 0xff}.${(ip >>> 16) & 0xff}.${(ip >>> 8) & 0xff}.${ip & 0xff}` as IPAddress;
+  // Credit goes to yichizhng on BitBurner discord
+  // Generates a number like 0.c8f0a07f1d47e8
+  const ip = Math.random().toString(16);
+  // uses regex to match every 2 characters. [0.][c8][f0][a0][7f][1d][47][e8]
+  const matchArray = ip.match(/../g) as RegExpMatchArray;
+  // we only want #1 through #4
+  const sliced = matchArray.slice(1, 5);
+  //convert each to a decimal number
+  const asDecimals = sliced.map((x) => parseInt(x, 16));
+  // and join them together to make a human readable IP address.
+  return asDecimals.join(".") as IPAddress;
 };
